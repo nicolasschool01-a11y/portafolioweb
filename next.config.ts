10121d62b0103c19956ec161f01832f234c6cc05
Nicolas <nico@MacBook-Pro-de-Nicolas.local>
2026-04-22 12:32:05 -0300

    despliegue automático

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: false,
   async rewrites() {
     return [
